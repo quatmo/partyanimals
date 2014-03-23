97d5399beeaaa6d242deff13cabfd355f598d5b0
Julius Cebreros <me@juliuscebreros.com>
2014-03-23 23:04:35 +0800

    Added main screens

---
+++ Assets/Scripts/DistrictBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistrictBehaviour : MonoBehaviour {
+
+	public iTween.EaseType easeType;
+
+	// Use this for initialization
+	void Start () {
+		var random = Random.Range(.7f, 1.5f);
+		iTween.MoveFrom(gameObject, iTween.Hash ("y", 2, "time", random, "easetype", easeType));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MainMenuControls.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuControls : MonoBehaviour {
+	
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(Screen.width/2, Screen.height/2, 80, 20), "Start")){
+			Application.LoadLevel("Game");
+		}
+	}
+}
