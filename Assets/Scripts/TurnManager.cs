d443723536e368d7342b24c111e4a20e648e07e9
Julius Cebreros <me@juliuscebreros.com>
2014-05-04 22:29:19 +0800

    Refactored camera controls and turn controls

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class TurnManager : MonoBehaviour {
 
+	private CameraController cameraController;
+	private DistrictManager districtManager;
+
 	// Use this for initialization
 	void Start () {
+		cameraController = GetComponent<CameraController>();
+		districtManager = GetComponent<DistrictManager>();
 		// Check if this is the start of a game
 		if(GameManager.GetInstance().IsNewGame){
 			//show district selection screen
@@ -16,5 +21,27 @@ public class TurnManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	[Signal]
+	void onClick(DistrictBehaviour districtBehaviour){
+		Vector3 districtPosition = districtBehaviour.gameObject.transform.position;
+		DistrictData districtData = districtBehaviour.districtData;
+
+		GameManager.GetInstance().setSelectedDistrict(districtData);
+
+		cameraController.zoomIn();
+		cameraController.tweenTo(districtPosition.x, districtPosition.y);
+
+		//what state of the game is this?
+
+		//if in my turn
+		districtManager.showDistrictDataWindow(districtData);
+	}
+
+	[Signal]
+	void onSortieClicked()
+	{
+		Application.LoadLevel("Sortie");
 	}
 }
